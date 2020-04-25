4fb7345ee1fd4b8c857f138e9ea0d6ce12734c81
Ken Galliher <kgalliher@esri.com>
2020-04-25 13:38:51 -0700

    Add excel to db script

--- port-excel.py
+++ port-excel.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import sqlite3
 
 df = pd.read_excel("deliveries.xlsx")
 print(df)
+
+# Clean up column names
+df = df.rename(columns={'My House?': 'MyHouse'})
+clean_df = df[["Date", "Company", "MyHouse"]]
+
+conn = sqlite3.connect("deliveries.sqlite")
+
+# move df to db
+clean_df.to_sql("deliveries", conn, if_exists="append")
