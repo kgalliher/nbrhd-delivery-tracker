dcb146ab8322e7543f594533d27ca8bce9bcb494
Ken Galliher <kgalliher@esri.com>
2020-04-25 18:37:57 -0700

    Add while for multiple entries

--- add-delivery.py
+++ add-delivery.py
@@ -3,25 +3,29 @@ import sys
 import datetime
 import pandas as pd
 
-_date = input("Enter the date ('x' for current tme):  ")
-if _date == "x":
-    _date = datetime.datetime.now()
 
-_company = input("Enter delivery company name:  ")
-my_house = input("Is the delivery for you?  ")
+while True:
+    _date = input("Enter the date ('x' for current time, 'q' to exit):  ")
+    if _date == "x":
+        _date = datetime.datetime.now()
+    if _date == "q":
+        break
 
-print(_date, _company, my_house)
-confirm = input("Is this correct?")
+    _company = input("Enter delivery company name:  ")
+    my_house = input("Is the delivery for you?  ")
 
-if confirm.upper() == "YES" or confirm.upper() == "Y":
-    conn = sqlite3.connect(
-        r"C:\Users\kgall\OneDrive\Documents\covid-deliveries\deliveries.sqlite")
-    curse = conn.cursor()
-    curse.execute("INSERT INTO deliveries (Date, Company, MyHouse) VALUES (?, ?, ?)", [
-                  _date, _company, my_house])
-    conn.commit()
+    print(_date, _company, my_house)
+    confirm = input("Is this correct?")
 
-    df = pd.read_sql("SELECT id, Company FROM deliveries", conn)
-    print(df.groupby(["Company"]).count())
-else:
-    print("Exiting")
+    if confirm.upper() == "YES" or confirm.upper() == "Y":
+        conn = sqlite3.connect(
+            r"C:\Users\kgall\OneDrive\Documents\covid-deliveries\deliveries.sqlite")
+        curse = conn.cursor()
+        curse.execute("INSERT INTO deliveries (Date, Company, MyHouse) VALUES (?, ?, ?)", [
+            _date, _company, my_house])
+        conn.commit()
+
+        df = pd.read_sql("SELECT id, Company FROM deliveries", conn)
+        print(df.groupby(["Company"]).count())
+    else:
+        print("Exiting")
